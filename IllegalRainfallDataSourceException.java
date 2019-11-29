24562f9f5b363a8b4c4254cd370574d7c49ef423
RianWoods <56262155+RianWoods@users.noreply.github.com>
2019-11-29 15:34:15 +0000

    Add files via upload
    javaDoc Commented

---
+++ IllegalRainfallDataSourceException.java
@@ -0,0 +1,12 @@
+package cs3318.exceptions;
+import java.lang.Exception;
+
+/**
+ * IllegalRainfallDataSourceException clas definition
+ * Unnecessary to define additional exceptions
+ */
+public class IllegalRainfallDataSourceException extends Exception {
+    public IllegalRainfallDataSourceException(){
+        System.ou.println("Illegal rainfall data source.");
+    }
+}
